976d71f9d797917d47eb58df3e5b8622fbb7904d
Ilya <godfrid7@mail.ru>
2022-11-20 12:38:42 +0300

    src/main.java

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 2000;
+        int bonus = 20;
+        int mile = 1;
+
+        int sale = ticket / bonus * mile;
+
+        System.out.println("Начислено милей: " + sale);
+
+    }
+}
